f0fa2fe69475e1e169126b5de8602082d51cd5ed
anpenta <anpenta01@gmail.com>
2019-10-25 17:55:02 +0200

    create load data function

--- code/utility.py
+++ code/utility.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import pandas as pd
+import os
 
 
 def plot(image, image_name):
@@ -11,10 +13,52 @@ def plot(image, image_name):
 def read_image(image_path):
   return cv.imread(image_path, cv.IMREAD_UNCHANGED)
 
+
+def load_data(data_path):
+  print("loading data")
+
+  # Determine the suffixes of the files.
+  image_suffix = ".png"
+  pos_suffix = "pos.txt"
+  neg_suffix = "neg.txt"
+
+  # Load the images and rectangles.
+  images = pd.DataFrame(columns=["filenames", "images"])
+  pos_rectangles = pd.DataFrame(columns=["filenames", "x", "y"])
+  neg_rectangles = pd.DataFrame(columns=["filenames", "x", "y"])
+  for filename in os.listdir(data_path + "/"):
+      file_path = data_path + "/" + str(filename)
+
+      if str(filename).endswith(image_suffix):
+        new_row = [str(filename), read_image(file_path)]
+        images.loc[len(images)] = new_row
+
+      if str(filename).endswith(pos_suffix) or str(filename).endswith(neg_suffix):
+        points = pd.read_csv(file_path, sep=" ", header=None)
+
+        # Remove any extra columns with NaN values that should not be read.
+        if len(points.columns) > 2:
+          for i in range(2, len(points.columns)):
+            del points[i]
+
+        points.columns = ["x", "y"]
+        points.insert(0, "filenames", str(filename))
+
+        if str(filename).endswith(pos_suffix):
+          pos_rectangles = pos_rectangles.append(points)
+        else:
+          neg_rectangles = neg_rectangles.append(points)
+
+  return images, pos_rectangles, neg_rectangles
+
+
 def test():
-  image_path = "../dataset/pcd0100r.png"
-  image_name = "test"
-  image = read_image(image_path)
-  plot(image, image_name)
+  path = "../test"
+
+  x, y, z = load_data(path)
+  print(y)
+  print(z)
+  for i, j in x.iterrows():
+    plot(j["images"], j["filenames"])
 
 test()
