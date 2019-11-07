dc8e791ea11fe53834490404a8b8ddd67ff95bb7
mrcabo <dcgdiego@gmail.com>
2019-11-07 22:42:28 +0100

    script to download the whole dataset

---
+++ download_dataset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Downloading dataset from http://pr.cs.cornell.edu/grasping/rect_data/data.php"
+
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data01.tar.gz -o data01.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data02.tar.gz -o data02.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data03.tar.gz -o data03.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data04.tar.gz -o data04.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data05.tar.gz -o data05.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data06.tar.gz -o data06.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data07.tar.gz -o data07.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data08.tar.gz -o data08.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data09.tar.gz -o data09.tar.gz
+curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data10.tar.gz -o data10.tar.gz
