7424f4cbef0f5db9d87bbdbaef36bace38669da2
mrcabo <dcgdiego@gmail.com>
2019-11-08 19:07:42 +0100

    Trying to fix the problem, let's see if it works..

--- code/train.py
+++ code/train.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from utility import load_data
 from preprocess import to_five_dimensional, split_train_test_data, replicate_images, normalize_pix_val
 
@@ -11,13 +13,15 @@ x_train, y_train, x_test, y_test = split_train_test_data(replicated_images, pos_
 
 x_train, x_test = normalize_pix_val(x_train), normalize_pix_val(x_test)
 
+aux = x_train['images'].to_numpy()
+aux = [img for img in aux]
+x_train = np.asarray(aux)
+# print("Shape of x_train: {}".format(x_train.shape))
+y_train = y_train.loc[:, ['center_x', 'center_y', 'width', 'height', 'angle']].to_numpy()
+# print("Shape of y_train: {}".format(y_train.shape))
+
 model = ResNet50(pretrained=True)
 model.summary()
 
-import numpy as np
-x_train = x_train["images"]
-x_train = np.array([x for x in x_train])
-del y_train["filenames"]
-
-model.train(x_train, y_train, epochs=20, batch_size=1, validation_split=0.1, output_path="./output")
-#model.test(x_train, y_train, "./output", batch_size=1)
+model.train(x_train, y_train, epochs=20, batch_size=32, validation_split=0.1, output_path="./output")
+# model.test(x_train, y_train, "./output", batch_size=1)
