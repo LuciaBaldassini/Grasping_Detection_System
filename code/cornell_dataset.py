35655fc8f7c4e50b3194c89a092e7c858e79d855
mrcabo <dcgdiego@gmail.com>
2019-11-09 21:30:14 +0100

    Smaller fixes

--- code/cornell_dataset.py
+++ code/cornell_dataset.py
@@ -44,10 +44,19 @@ class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        image, landmarks = sample['image'], sample['rectangle']
+        image, rectangle = sample['image'], sample['rectangle']
 
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
-        return {'image': torch.from_numpy(image), 'rectangle': torch.from_numpy(landmarks)}
+        return {'image': torch.from_numpy(image), 'rectangle': torch.from_numpy(rectangle)}
+
+
+class Normalize(object):
+    """Normalizes values between -1 and 1."""
+
+    def __call__(self, sample):
+        image, rectangle = sample['image'], sample['rectangle']
+        image = image / 255.0
+        return {'image': image, 'rectangle': rectangle}
--- code/pytorch_approach.py
+++ code/pytorch_approach.py
@@ -2,8 +2,10 @@ from __future__ import print_function, division
 from pathlib import Path
 import pandas as pd
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
-from cornell_dataset import CornellDataset, ToTensor
+from cornell_dataset import CornellDataset, ToTensor, Normalize
+from util import plot_image
 
 # PATH_TO_DATA = Path('/home/diego/Documents/RUG/CognitiveRobotics/Grasping_Detection_System/dataset')
 PATH_TO_DATA = Path('/home/diego/Documents/RUG/CognitiveRobotics/Grasping_Detection_System/debug_dataset')
@@ -13,41 +15,24 @@ NUM_WORKERS = 4
 
 cpos_labels = pd.read_csv(PATH_TO_POS_LABELS, index_col=0)
 
-transformed_dataset = CornellDataset(PATH_TO_POS_LABELS.as_posix(), PATH_TO_DATA.as_posix(), transform=ToTensor())
+transformed_dataset = CornellDataset(PATH_TO_POS_LABELS.as_posix(), PATH_TO_DATA.as_posix(),
+                                     transform=transforms.Compose([
+                                         ToTensor(),
+                                         Normalize()
+                                     ]))
 
 dataloader = DataLoader(transformed_dataset, batch_size=BATCH_SIZE,
                         shuffle=True, num_workers=NUM_WORKERS)
 
 for i_batch, sample_batched in enumerate(dataloader):
-    aux = sample_batched['image'].numpy()[0]
-    print(i_batch, sample_batched['image'].size(), sample_batched['rectangle'].size())
+    for i, image in enumerate(sample_batched['image']):
+        rect = sample_batched['rectangle'][i].numpy()
+        image = image.numpy().transpose((1, 2, 0))
+        print("For i_batch {}, image_idx {}: {} {}".format(i_batch, i, image.shape, rect.shape))
+        plot_image(image, rect)
 
+    print(i_batch, sample_batched['image'].size(), sample_batched['rectangle'].size())
+    if i_batch >= 1:
+        break
 
 print("Bye")
-
-# for i in range(len(transformed_dataset)):
-#     sample = transformed_dataset[i]
-#
-#     print(i, sample['image'].size(), sample['rectangle'].size())
-#
-#     if i == 3:
-#         break
-
-
-# fig = plt.figure()
-# for i in range(len(dataset)):
-#     sample = dataset[i]
-#
-#     print(i, sample['image'].shape, sample['rectangle'].shape)
-#
-#     ax = plt.subplot(1, 4, i + 1)
-#     plt.tight_layout()
-#     ax.set_title('Sample #{}'.format(i))
-#     ax.axis('off')
-#     # image, rectangle = sample
-#     plot_image(**sample)
-#     # show_landmarks(**sample)
-#
-#     if i == 3:
-#         plt.show()
-#         break
